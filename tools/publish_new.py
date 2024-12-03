a80ddbf742bf50cda8aec09a3857c3d5cf72e4c4
imx666 <3490980474@qq.com>
2024-12-04 01:00:00 +0800

    Update publish_new.py

--- tools/publish_new.py
+++ tools/publish_new.py
@@ -7,11 +7,14 @@ from celery import Celery
 import os
 from dotenv import load_dotenv
 
-# 指定.env.dev文件的路径
-dotenv_path = os.path.join(os.getcwd(), '.env.dev')
-print(os.getcwd())
+from pathlib import Path
+project_path = Path(__file__).resolve().parent  # 此脚本的运行"绝对"路径
+# project_path = os.getcwd()  # 此脚本的运行的"启动"路径
+dotenv_path = os.path.join(project_path, '../.env.dev')  # 指定.env.dev文件的路径
+print(project_path)
+load_dotenv(dotenv_path)  # 载入环境变量
 
-load_dotenv(dotenv_path)
+
 print(os.getenv('WEBHOOK_SECRET'))
 
 
@@ -26,8 +29,13 @@ REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")
 REDIS_PORT = os.getenv("REDIS_PORT")
 REDIS_CELERY_DB_CICD = os.getenv("REDIS_CELERY_DB_CICD")
 broker_url = f'redis://:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}/{REDIS_CELERY_DB_CICD}'
+print(broker_url)
 app_worker = Celery('webhook', broker=broker_url, backend=broker_url)
 
 a='12345678'
-app_worker.send_task('webhook.auto_deploy', args=[a])
+result = app_worker.send_task('webhook.auto_deploy', args=[a])
 
+# 检查任务是否完成
+print(result.ready())
+# 获取任务结果
+print(result.get())
